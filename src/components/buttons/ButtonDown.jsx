af60e3159dd6f04db5163939860ffc4f30e9caf9
Joshua Candia <joshuacandia74@gmail.com>
2023-09-04 11:41:34 -0300

    fix(header/buttonDown): buttondown padding

--- src/components/buttons/ButtonDown.jsx
+++ src/components/buttons/ButtonDown.jsx
@@ -2,13 +2,16 @@ import { BsArrowDownShort } from 'react-icons/bs';
 
 const ButtonDown = () => {
   const scrollDown = () => {
+    const viewportHeight = window.innerHeight;
+    const scrollDistance = 0.98 * viewportHeight;
+
     window.scrollTo({
-      top: document.body.scrollHeight,
+      top: scrollDistance,
       behavior: 'smooth',
     });
   };
   return (
-    <button onClick={scrollDown} className='cursor-pointer'>
+    <button onClick={scrollDown} className='mb-6 cursor-pointer'>
       <BsArrowDownShort className='text-neutral-800 animate-bounce' size={50} />
     </button>
   );
