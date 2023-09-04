03528b5ac8bc7e43dc0f5a917ffe4cd67b2319c2
Joshua Candia <joshuacandia74@gmail.com>
2023-09-04 00:42:22 -0300

    prettier and eslint setted

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Linda Rico</h1>
+      <h1 className='text-3xl font-bold underline'>Linda Rico</h1>
     </div>
   );
 }
