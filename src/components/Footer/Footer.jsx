657b6f85273ece3a07bdac00b535aec1cfe616b3
Joshua Candia <joshuacandia74@gmail.com>
2023-09-06 11:31:33 -0300

    enterprise name and font style changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,33 @@
 import ButtonContact from '../buttons/ButtonContact';
 import { SiCountingworkspro } from 'react-icons/si';
-import {AiOutlineMail,AiOutlinePhone} from "react-icons/ai"
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <footer className='flex flex-col'>
       <section className='bg-red-400 h-[60vh] flex flex-col justify-center items-center gap-7 bg-[url("../src/assets/contact-bg.jpg")] bg-cover xl:h-[400px] xl:bg-center'>
         <div className='flex flex-col justify-center items-center gap-7 p-7 h-full backdrop-blur-md w-full text-white'>
-        <div className='flex items-center gap-2 '>
-          <SiCountingworkspro className='text-[27px]' />
-          <p className='font-bold text-[24px]'>Linda Rico</p>
+          <div className='flex items-center gap-2 '>
+            <SiCountingworkspro className='text-[27px]' />
+            <p className='font-bold text-[24px]'>LRA Consultora</p>
+          </div>
+          <div className='flex items-center gap-2 '>
+            <AiOutlinePhone className='text-[27px]' />
+            <p className='font-bold text-[24px]'>(+549) 11 3488 8325</p>
+          </div>
+          <div className='flex items-center gap-2 '>
+            <AiOutlineMail className='text-[27px]' />
+            <p className='font-bold text-[24px]'>lindarico.seh@hotmail.com</p>
+          </div>
+          <ButtonContact />
         </div>
-        <div className='flex items-center gap-2 '>
-          <AiOutlineMail className='text-[27px]' />
-          <p className='font-bold text-[24px]'>mail@mail.com</p>
-        </div>
-        <div className='flex items-center gap-2 '>
-          <AiOutlinePhone className='text-[27px]' />
-          <p className='font-bold text-[24px]'>12345-6789</p>
-        </div>
-        <ButtonContact/>
-        </div>
-      
-        
-      
       </section>
-      <section className='bg-BlueLight h-[20vh] flex flex-col justify-center items-center p-4 '>
+      <section className='bg-BlueLight h-[20vh] flex flex-col justify-center items-center p-4 gap-2'>
         <p className='text-white text-sm'>
           Copyright © 2023-All Rights Reserved.
         </p>
         <p className='text-white text-sm'>
-        Linda Rico - Seguridad e
-          Higiene
+          LRA - Consultoría en Seguridad e Higiene
         </p>
       </section>
     </footer>
--- src/components/Services/HeaderServices.jsx
+++ src/components/Services/HeaderServices.jsx
@@ -1,12 +1,13 @@
-import ButtonContact from "../buttons/ButtonContact";
+import ButtonContact from '../buttons/ButtonContact';
 const HeaderServices = () => {
-  return <section className=" flex flex-col gap-7 justify-center items-center bg-[url('../../src/assets/mujer-notebook.jpg')]  bg-[length:900px] bg-fixed bg-no-repeat h-[400px]">
-    <div className="flex flex-col items-center justify-center gap-7 h-full w-full backdrop-blur-[2px]">
-    <h1 className="text-4xl font-bold">Servicios</h1>
-   <ButtonContact/>
-    </div>
-   
-  </section>;
+  return (
+    <section className=" flex flex-col gap-7 justify-center items-center bg-[url('../../src/assets/mujer-notebook.jpg')]  bg-[length:900px] bg-fixed bg-no-repeat h-[400px]">
+      <div className='flex flex-col items-center justify-center gap-7 h-full w-full backdrop-blur-[2px]'>
+        <h1 className='font-customFontInter text-4xl font-bold'>Servicios</h1>
+        <ButtonContact />
+      </div>
+    </section>
+  );
 };
 
 export default HeaderServices;
